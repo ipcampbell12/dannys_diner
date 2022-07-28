4e851757c6f643244e065b6386eca7a70a6bd190
ipcampbell12 <ipcampbell12@gmail.com>
2022-07-28 10:25:56 -0700

    july 28th

--- ddiner.sql
+++ ddiner.sql
@@ -38,3 +38,39 @@ GROUP BY customer_id;
 
 
 
+
+
+	--Window functio practice -- 
+	-- Find the size of each employee team 
+	SELECT employee_id,
+		--count how many employees are in each team--
+		COUNT(employee_id) OVER (PARTITION BY team_id ORDER BY team_id) AS team_size
+	FROM employee;
+
+
+	--find the top three highest paid employees from each department
+
+	--window function as a subquery
+	SELECT department,employee, salary
+	(SELECT e.id, 
+		e.name AS employee,
+		e.Salary AS salary,
+		d.name AS department,
+		--grooups them by department and orders them by salary, hightest to lowest
+		DENSE_RANK() OVER (PARTITION BY d.id ORDER BY Salary DESC ) AS rank
+	FROM employee e
+	JOIN Department d
+	ON e.departmentId = d.departmentId ) AS sub
+	WHERE rank <= 3
+
+
+--Analytic functionS
+--check which users have logged in for at least 5 days in a row 
+SELECT 
+	id,
+	login_date,
+	--use 4 because that is the difference between first day and 5th day
+	LAG(login_date,4) over (PARTITION BY id ORDER BY login_date) as lag4
+FROM logins
+
+
