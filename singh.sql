5ce02edff12d9e1833439a6f8e4264aa110ce63b
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-22 14:54:28 -0700

    signup activation rate

--- singh.sql
+++ singh.sql
@@ -245,3 +245,11 @@ WITH supercloud_cte AS(
   SELECT customer_id 
   FROM supercloud_cte
   WHERE category_count = 3 
+
+
+--SIGNUP ACTIVATION rate
+SELECT
+  ROUND((COUNT(t.email_id)::DECIMAL/COUNT(DISTINCT e.email_id) ),2) AS activation_rate
+FROM emails e 
+LEFT JOIN texts t
+ON e.email_id = t.email_id AND t.signup_action = 'Confirmed'
